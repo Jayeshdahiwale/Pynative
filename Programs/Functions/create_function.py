db1eb6e8b26a5b1ee46673838fc358ae60faab3e
JAYESH <jayjdahivale@gmail.com>
2022-04-22 17:01:33 +0530

    [Jayesh]:Added

---
+++ Programs/Functions/create_function.py
@@ -0,0 +1,17 @@
+"""
+
+@Author: Jayesh Dahiwale
+@Date: 2022-04-13 07:38:00
+@Last Modified by: Jayesh Dahiwale
+@Last Modified time: 2022-04-12 19:00:00
+@Title : Create a function
+
+"""
+# demo is the function name
+def demo(name, age):
+    # print value
+    print(name, age)
+
+# call function
+if __name__ == "__main__":
+    demo("Jayesh", 25)
