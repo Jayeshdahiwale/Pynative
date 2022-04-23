f3831500a14e8275745928d6aacc8965770d291b
JAYESH <jayjdahivale@gmail.com>
2022-04-23 14:49:26 +0530

    [Jayesh]:Added

---
+++ Programs/OOPS/class_inheritance.py
@@ -0,0 +1,29 @@
+"""
+
+@Author: Jayesh Dahiwale
+@Date: 2022-04-13 07:38:00
+@Last Modified by: Jayesh Dahiwale
+@Last Modified time: 2022-04-12 19:00:00
+@Title : Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle
+is seating capacity * 100. If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance
+charge. So total fare for bus instance will become the final amount =  total fare + 10% of the total fare.
+
+"""
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10 / 100
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
