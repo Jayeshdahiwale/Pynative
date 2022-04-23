b051824cb3d176c7d67b3369cc835c58f5146dd3
JAYESH <jayjdahivale@gmail.com>
2022-04-23 13:11:31 +0530

    Added

---
+++ Programs/Functions/inner_outer_function.py
@@ -0,0 +1,24 @@
+"""
+
+@Author: Jayesh Dahiwale
+@Date: 2022-04-13 07:38:00
+@Last Modified by: Jayesh Dahiwale
+@Last Modified time: 2022-04-12 19:00:00
+@Title : Working on the inner and outer function
+
+"""
+# outer function
+def outer_fun(a, b):
+    square = a ** 2
+
+    # inner function
+    def addition(a, b):
+        return a + b
+
+    # call inner function from outer function
+    add = addition(a, b)
+    # add 5 to the result
+    return add + 5
+if __name__ =="__main__":
+    result = outer_fun(5, 10)
+    print(result)
